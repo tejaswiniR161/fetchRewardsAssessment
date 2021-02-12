8227aa52afc4af19183c58f2775a41487adf3b1f
Tejaswini Rajkumar <tejr1601@gmail.com>
2021-02-12 10:07:47 -0500

    deductPoints API added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 addPoints API - Adding one record at once
+getPoints API - No timestamp mentioned - as per the example document
+deductPointsAPI - Not tracking -ve i/ps from the addPoints yet
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require("body-parser");
 var cors=require("cors");
+var moment=require("moment");
 var app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -8,8 +9,84 @@ app.use(bodyParser.json());
 totalPoints={};
 distributedPoints=[];
 
+app.post('/deductPoints',(req,res)=>{
+    //Algorithm is expected to 
+    //1) Remove points that were added in first
+    //2) No negative points for payers
+    var sum=0;
+    var reqpoints=req.body.points;
+    for(var val in totalPoints)
+    {
+        sum+=totalPoints[val];
+    }
+
+    if(sum<reqpoints)
+    {
+        res.send({"Message":"Available points is lesser than the deduction request"});
+    }
+
+    else
+    {
+        tempPoints=reqpoints;
+        remainingPoints=reqpoints;
+        deductedPoints={};
+
+        distributedPoints= distributedPoints.sort((a,b)=> { return new Date(b.time) - new Date(a.time) });
+        var ptr=0,ptr2=0;
+        modifiedDistributedPoints=distributedPoints;
+
+        while(remainingPoints>0)
+        {
+            currentPayer=distributedPoints[ptr];
+            currentDeduction=0;
+
+            if(deductedPoints[currentPayer["points"]]-remainingPoints>=0)
+            {
+                currentDeduction=currentPayer["points"]-remainingPoints;
+                console.log("So the remaining was not a -ve so... "+currentDeduction);
+            }
+
+            else
+            {
+                currentDeduction=currentPayer["points"];
+                console.log("So the thing was lesser so, ..."+currentDeduction);
+                modifiedDistributedPoints.splice(ptr2,1);
+                ptr2--;
+            }
+            
+            remainingPoints-=currentDeduction;
+            console.log("remainingPoints = ",remainingPoints);
+            currentDeduction*=-1;
+            //remainingPoints=reqpoints-tempPoints;
+            if(deductedPoints[currentPayer["payer"]])
+                deductedPoints[currentPayer["payer"]]-=currentDeduction;
+            else
+                deductedPoints[currentPayer["payer"]]=currentDeduction;
+            ptr++;
+            ptr2++;
+            
+            if(remainingPoints<=0)
+            {
+                distributedPoints=modifiedDistributedPoints;
+                for(var i in totalPoints)
+                {
+                    if(deductedPoints[i])
+                        totalPoints[i]+=deductedPoints[i];
+                }
+                res.send(deductedPoints);
+                console.log(distributedPoints);
+                console.log(totalPoints);
+            }
+        }        
+    }
+
+});
+
 app.post('/addPoints', (req, res)=>{
     record=req.body;
+    record.time=moment(record.time,"MM/DD/YYYY h:m a").toDate();
+
+    //console.log(record.time);
     //var found=false;
     distributedPoints.push(record);
         /* totalPoints.forEach(u => 
@@ -34,9 +111,6 @@ app.post('/addPoints', (req, res)=>{
         {
             totalPoints.push(record);
         } */
-
-    console.log(totalPoints);
-    console.log(distributedPoints);
     res.send(totalPoints);  
 });
 
